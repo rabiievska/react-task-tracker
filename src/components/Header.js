b132965f6c304f56a311763561dc5169838767ca
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-09-23 12:36:41 +0200

    Create a button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
+import { render } from '@testing-library/react';
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('hi');
+  }
+  
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
