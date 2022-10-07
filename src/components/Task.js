8eac07d31195c5da59b9eeb7c09d2e70e828fdbb
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-10-07 14:17:18 +0200

    Add the logic of the removing element

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,15 @@
-export const Task = ({ task }) => {
+import { FaTimes } from 'react-icons/fa'
+
+export const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
-      <h3>{task.text}</h3>
+      <h3>{
+        task.text} 
+        <FaTimes 
+          style={{ color: '#d80000', cursor: 'pointer'}}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
       <p>{task.day}</p>
     </div>
   )
