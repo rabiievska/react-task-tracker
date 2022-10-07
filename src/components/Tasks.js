c971965ccaa05f0f7f0976ce7fa835385320f822
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-10-07 13:41:43 +0200

    Create Task component, install react-icons

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,13 @@
+import Task from './Task'
 
 const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </>
   )
 }
-
-
 
 export default Tasks
