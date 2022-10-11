45210a897638221a09819cead1dbe2cfc72f2773
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-10-11 21:31:27 +0200

    Add reminder (map function is failing)

--- src/App.js
+++ src/App.js
@@ -29,11 +29,15 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)) //map function is failing
+  };
+  
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         <p>No Tasks</p>
       )}
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,10 @@
 import { FaTimes } from 'react-icons/fa'
 
-export const Task = ({ task, onDelete }) => {
+export const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
+    <div key={task.id} className={`task ${task.reminder === true ? 'reminder' : ''}`} onDoubleClick={() => 
+      onToggle(task.id)
+    } >
       <h3>{
         task.text} 
         <FaTimes 
