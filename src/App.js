20de2775526341d3bf828ca8b72f57fb3960d594
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-10-21 13:41:29 +0200

    Connect JSON server

--- src/App.js
+++ src/App.js
@@ -5,27 +5,7 @@ import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState(
-    [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Nov 5th at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at Office',
-        day: 'Nov 6th at 12:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Nov 5th at 18:30pm',
-        reminder: false,
-      }
-    ]);
+  const [tasks, setTasks] = useState([]);
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1;
