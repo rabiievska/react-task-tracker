ba9be6204129529d6b565eb43025d9a6c5b4e0e3
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-11-02 21:21:07 +0100

    Add task to server

--- src/App.js
+++ src/App.js
@@ -22,10 +22,18 @@ function App() {
     return data;
   };
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5002/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    });
+
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
   };
 
   const deleteTask = async (id) => {
